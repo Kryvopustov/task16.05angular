8a5d7da91d3ac1dc13cfb8de519711c09c1afd44
IGOR2 <i.kryvopustov@andersenlab.com>
2016-05-17 15:54:52 +0300

    Update

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -5,47 +5,31 @@
     .module('myApp.home')
     .controller('HomeController', HomeController);
 
-  HomeController.$inject = ['$scope', '$location', '$anchorScroll'];  
+  HomeController.$inject = ['$scope', '$location', '$anchorScroll'];
+
 
   function HomeController($scope, $location, $anchorScroll) {
+
     $scope.scrollTo = function(id) {
-        $location.hash(id);
-        $anchorScroll();
-    }
+      $anchorScroll.yOffset = 50;
+      $location.hash(id);
+      $anchorScroll(id);
+    };
+
+    $scope.loadScroll = function(){
+      var id = $location.hash();
+      if (id) {
+        $scope.scrollTo(id);
+      };
+    };
+ // $scope.$on('$viewContentLoaded', function(){
+ //      var test1 = $location.hash();
+ //      if (test1) {
+ //        $scope.scrollTo(test1)
+ //      };
+ //  });
+
+
+
   }
 })();
-
-/*
-        var start = 0;
-        var ending = start+10;
-        var lastdata = 100;
-        var reachLast = false;
-
-        $scope.loadmore = "Loading More data..";
-        $scope.testData = [];
-
-        $scope.listData = function() {
-            if(reachLast){
-                return false;
-            }
-            var jsondt = [];
-            for (var i = start; i < ending; i++) {
-                jsondt.push({
-                    id: i, name: "name"+i
-                });
-            };
-            start = i;
-            ending = i+10;
-
-            $scope.testData =$scope.testData.concat(jsondt);
-
-
-            if(ending >= lastdata) {
-                reachLast = true;
-                $scope.loadmore = "Reached at bottom";
-            }
-        };
-
-
-        $scope.listData();
-*/
